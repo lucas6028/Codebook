4f16263f6916c33e263e1c36ca892f8ed80a7e9b
Hao <144909142+lucas6028@users.noreply.github.com>
2025-09-26 05:43:35 +0000

    modify polygon area

--- codes/Geometry/Polygon area.cpp
+++ codes/Geometry/Polygon area.cpp
@@ -14,7 +14,9 @@
 template<class T>
 T polygonArea2(vector<Point<T>>& v) {
 	T a = v.back().cross(v[0]);
-	rep(i,0,sz(v)-1) a += v[i].cross(v[i+1]);
+    for (int i = 0; i < (int)v.size() - 1; ++i) {
+        a += v[i].cross(v[i+1]);
+    }
 	return a;
 }
 
