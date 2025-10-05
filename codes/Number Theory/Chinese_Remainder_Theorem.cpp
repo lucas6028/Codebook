75e182c68b2b4c27443375fb095d12fa2ea727fb
Hao <144909142+lucas6028@users.noreply.github.com>
2025-10-05 01:25:13 +0000

    modify codes/Number%20Theory/Chinese_Remainder_Theorem.cpp

--- codes/Number Theory/Chinese_Remainder_Theorem.cpp
+++ codes/Number Theory/Chinese_Remainder_Theorem.cpp
@@ -1,10 +1,18 @@
 // coprime (p^k)
 pair<ll, ll> CRT(const vector<pair<ll, ll>>& congruences) {
-    ll M{1}, sol{};
+    ll M = 1, sol = 0;
     for (auto& [m, a] : congruences) M *= m;
     for (auto& [m, a] : congruences) {
-        ll x{M / m}, y{MI(x, m)};
+        ll x = M / m, y = MI(x, m);
         sol = MA(sol, MM(MM(a, x, M), y, M), M);
     }
     return {M, sol};
+}
+
+// example usage
+int main() {
+    vector<pair<ll, ll>> congruences = {{3, 2}, {5, 3}, {7, 2}};
+    auto [M, sol] = CRT(congruences);
+    cout << "M: " << M << ", x: " << sol << endl; // M: 105, x: 23
+    return 0;
 }
