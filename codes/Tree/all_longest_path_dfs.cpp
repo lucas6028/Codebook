69b69b8d74fcd3d58b3b74be9f8662e7f50d6a8b
Hao <144909142+lucas6028@users.noreply.github.com>
2025-11-08 02:20:00 +0000

    update all longest path

--- codes/Tree/all_longest_path_dfs.cpp
+++ codes/Tree/all_longest_path_dfs.cpp
@@ -1,36 +1,56 @@
 // all longest path (generalization of the tree diameter problem)
 vector<tuple<int, int, int>> dp{};
 // [mx1, x, mx2] the path of mx1 goes through x
-int dfs1(int u, int w = -1) {
-    int mx{0};
-    for (auto& v : adj[u])
-        if (v != w) {
-            auto l{1 + dfs1(v, u)};
-            mx = max(mx, l);
 
-            auto& [mx1, x, mx2]{dp[u]};
-            if (l >= mx1) mx2 = mx1, mx1 = l, x = v;
-            else if (l > mx2) mx2 = l;
+int dfs1(int u, int p = -1) {
+    int mx = 0;
+    for (int v : adj[u]) {
+        if (v != p) {
+            int len = 1 + dfs1(v, u);
+            mx = max(mx, len);
+
+            auto& [mx1, x, mx2] = dp[u];
+            if (len >= mx1) {
+                mx2 = mx1;
+                mx1 = len;
+                x = v;
+            }
+            else if (len > mx2) {
+                mx2 = len;
+            }
         }
+    }
     return mx;
 }
-void dfs2(int u, int w = -1) {
-    if (w != -1) {
+
+void dfs2(int u, int p = -1) {
+    if (p != -1) {
         int tmx;
-        { // calculate the longest path through parent
-            auto& [mx1, x, mx2]{dp[w]};
+        {   // calculate the longest path through parent
+            auto& [mx1, x, mx2] = dp[p];
             if (x != u) tmx = mx1 + 1;
             else tmx = mx2 + 1;
         }
-        { // update the path
-            auto& [mx1, x, mx2]{dp[u]};
-            if (tmx >= mx1) mx2 = mx1, mx1 = tmx, x = w;
-            else if (tmx > mx2) mx2 = tmx;
+        {   // update the path
+            auto& [mx1, x, mx2] = dp[u];
+            if (tmx >= mx1) {
+                mx2 = mx1;
+                mx1 = tmx;
+                x = p;
+            }
+            else if (tmx > mx2) {
+                mx2 = tmx;
+            }
         }
     }
-    for (auto& v : adj[u])
-        if (v != w) dfs2(v, u);
+    for (int v : adj[u]) {
+        if (v != p) {
+            dfs2(v, u);
+        }
+    }
 }
+
 void all_longest_path() {
+    dp.resize(n);
     dfs1(0), dfs2(0);
 }
